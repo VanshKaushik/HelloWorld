d81a0a2694034615d91fa934d57dcd164062cdf9
Vansh Kaushik <vanshka@amdocs.com>
2023-10-18 07:29:49 +0530

    added heelo worlds text

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 // Your First Program
 
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+    public static void main(String[] args) { 
 	System.out.println("Hi my name is!");
+	System.out.println("Hello Worlds");
     }
 }
